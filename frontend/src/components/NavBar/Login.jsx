a929c07a9035f60162d8188a9458128c6b8e1133
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-02 20:27:11 +0800

    prevent redirect when user signs in with email, overwrite firebase box-shadow style in email signin flow

--- frontend/src/components/NavBar/Login.jsx
+++ frontend/src/components/NavBar/Login.jsx
@@ -2,75 +2,98 @@ import React from "react";
 import { Button, Modal, Nav } from "react-bootstrap";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import { default as firebase } from "../../firebase";
+import "./Login.css";
 
-const uiConfig = {
-  signInFlow: "popup",
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-  ],
-  callbacks: {
-    signInSuccessWithAuthResult: async (authResult, redirectUrl) => {
-      const idToken = await firebase
-        .auth()
-        .currentUser.getIdToken()
-        .then((idToken) => {
-          return idToken;
-        });
-      if (authResult.additionalUserInfo.isNewUser) {
-        await fetch("http://localhost:4000/users/createUser", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${idToken}`,
-          },
-          body: JSON.stringify({
-            userName: authResult.user.displayName,
-            email: authResult.user.email,
-            phoneNum: authResult.user.phoneNumber,
-            authUid: authResult.user.uid,
-          }),
-        });
-      }
-      return true;
-    },
-  },
-};
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      setShowLoginModal: false,
+    };
+  }
 
-function Login() {
-  const [showLoginModal, setShowLoginModal] = React.useState(false);
-  const handleCloseLoginModal = () => setShowLoginModal(false);
-  const handleShowLoginModal = () => setShowLoginModal(true);
-  return (
-    <>
-      <Nav.Link
-        variant="primary"
-        onClick={() => {
-          handleShowLoginModal();
-        }}
-      >
-        Log in
-      </Nav.Link>
+  handleShowLoginModal = () => {
+    this.setState({
+      setShowLoginModal: true,
+    });
+  };
 
-      <Modal show={showLoginModal} onHide={handleCloseLoginModal}>
-        <Modal.Body>
-          <StyledFirebaseAuth
-            uiConfig={uiConfig}
-            firebaseAuth={firebase.auth()}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            variant="outline-secondary"
-            size="sm"
-            onClick={handleCloseLoginModal}
-          >
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+  handleCloseLoginModal = () => {
+    this.setState({
+      setShowLoginModal: false,
+    });
+  };
+
+  render() {
+    var uiConfig = {
+      signInFlow: "popup",
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ],
+      credentialHelper: "none",
+      callbacks: {
+        signInSuccessWithAuthResult: async (authResult, redirectUrl) => {
+          const idToken = await firebase
+            .auth()
+            .currentUser.getIdToken()
+            .then((idToken) => {
+              return idToken;
+            });
+          if (authResult.additionalUserInfo.isNewUser) {
+            await fetch("http://localhost:4000/users/createUser", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${idToken}`,
+              },
+              body: JSON.stringify({
+                userName: authResult.user.displayName,
+                email: authResult.user.email,
+                phoneNum: authResult.user.phoneNumber,
+                authUid: authResult.user.uid,
+              }),
+            });
+          }
+          return true;
+        },
+      },
+    };
+
+    return (
+      <>
+        <Nav.Link
+          variant="primary"
+          onClick={() => {
+            this.handleShowLoginModal();
+          }}
+        >
+          Log in
+        </Nav.Link>
+
+        <Modal
+          show={this.state.setShowLoginModal}
+          onHide={this.handleCloseLoginModal}
+        >
+          <Modal.Body>
+            <StyledFirebaseAuth
+              uiConfig={uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="outline-secondary"
+              size="sm"
+              onClick={this.handleCloseLoginModal}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
 }
 
 export default Login;
