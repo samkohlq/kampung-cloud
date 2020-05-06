78f93f66a24d775d0396cbf04f5fdd9a4d21a8c0
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-06 23:39:56 +0800

    make editorial changes to safety guidelines for clarity

--- frontend/src/components/GettingStartedPage/GettingStartedPage.jsx
+++ frontend/src/components/GettingStartedPage/GettingStartedPage.jsx
@@ -1,83 +1,57 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import NavBar from "../NavBar/NavBar";
+import SafetyGuidelines from "./SafetyGuidelines";
 
 class AboutPage extends React.Component {
   render() {
     return (
       <>
         <NavBar />
-        <Row className="my-5">
-          <Col className="mx-5" xs={10} md={7}>
-            <h3 className="my-3">How this Kampung works</h3>
-            <p>This kampung is a community built on trust.</p>
-            <p>
-              We want to keep the kampung spirit alive and provide a safe haven
-              for those who want to help and those who need help. We know that
-              there are many who want to help but sometimes, just aren’t sure
-              where to start. That’s what we’re here for.
-            </p>
-          </Col>
-        </Row>
-        <Row className="my-5">
-          <Col className="mx-5" xs={10} md={7}>
-            <h3 className="my-3">How are requests fulfilled?</h3>
-            <p>
-              Request made > Request being prepared > Request on its way >
-              Request fulfilled
-            </p>
-          </Col>
-        </Row>
-        <Row className="my-5">
-          <Col className="mx-5" xs={10} md={7}>
-            <h3 className="my-3">Keeping this Kampung safe</h3>
-            <p>
-              The safety of our users is important to us. We don’t vet the
-              requests coming in because we trust that everybody is here out of
-              the goodness of their hearts and won’t take advantage of each
-              other. That being said, here are some safety guidelines to follow
-              just in case:
-            </p>
-            <h6>When receiving requests:</h6>
-            <p>
-              <ul>
-                <li>
-                  Call the person fulfilling your request to make sure it is not
-                  a scam
-                </li>
-                <li>
-                  If you’re not comfortable giving your full address, you can
-                  arrange to meet at a common area (e.g. car park, void deck)
-                </li>
-                <li>
-                  Wear a mask if you are picking up your item in person and
-                  maintain a safe distance
-                </li>
-                <li>
-                  You can have your requests left at the front door for a
-                  contactless pick-up
-                </li>
-                <li>Do not let a stranger into your house</li>
-              </ul>
-            </p>
-            <h6>When delivering requests:</h6>
-            <p>
-              <ul>
-                <li>
-                  Call the person before fulfilling the request to make sure it
-                  is not a scam.
-                </li>
-                <li>Wear a mask when making the delivery.</li>
-                <li>
-                  If you’re meeting in person, make sure to maintain a safe
-                  distance.
-                </li>
-                <li>
-                  You can leave the item at the door for a contactless delivery.
-                </li>
-                <li>Do not enter a stranger’s house.</li>
-              </ul>
-            </p>
+        <Row>
+          <Col md={{ offset: 2, span: 8 }}>
+            <Container>
+              <Row className="my-5">
+                <Col className="mx-5">
+                  <div className="text-center">
+                    <h3 className="my-3">How this Kampung works</h3>
+                    <p>This kampung is a community built on trust.</p>
+                    <p>
+                      We want to keep the kampung spirit alive and provide a
+                      safe haven for those who want to help and those who need
+                      help. We know that there are many who want to help but
+                      sometimes, just aren’t sure where to start. That’s what
+                      we’re here for.
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+              <Row className="my-5">
+                <Col className="mx-5">
+                  <div className="text-center">
+                    <h3 className="my-3">How are requests fulfilled?</h3>
+                    <p>
+                      Request made > Request being prepared > Request on its way
+                      > Request fulfilled
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+              <Row className="my-5">
+                <Col className="mx-5">
+                  <div className="text-left">
+                    <h3 className="my-3">Keeping this Kampung safe</h3>
+                    <p>
+                      The safety of our users is important to us. We don’t vet
+                      the requests coming in because we trust that everybody is
+                      here out of the goodness of their hearts and won’t take
+                      advantage of each other.
+                    </p>
+                    <SafetyGuidelines />
+                  </div>
+                </Col>
+              </Row>
+            </Container>
           </Col>
         </Row>
       </>
---
+++ frontend/src/components/GettingStartedPage/SafetyGuidelines.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+class SafetyGuidelines2 extends React.Component {
+  render() {
+    return (
+      <p className="text-left mt-5">
+        <h5 className="mb-3">
+          That being said, here are some safety guidelines to follow:
+        </h5>
+        <ul>
+          <li className="my-3">
+            Call the person making the request to check that it is not a scam
+          </li>
+          <li className="my-3">
+            Arrange for requests to be left at the front door for a contactless
+            pick-up, if possible
+          </li>
+          <li className="my-3">Otherwise, arrange to meet at a common area</li>
+          <li className="my-3">
+            Wear a mask if you are out and maintain a safe distance
+          </li>
+          <li className="my-3">Do not let a stranger into your house</li>
+        </ul>
+      </p>
+    );
+  }
+}
+
+export default SafetyGuidelines2;
