7fd80e1b5db0afdfad10c4df1071e357b2bc37cd
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-08 14:53:46 +0800

    add middleware to backend to allow requests from frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://kampung-cloud-prod.web.app"
+  );
+  // request methods to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // request headers to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  next();
+});
+
 app.use("/users", userRouter);
 app.use("/requests", requestRouter);
 app.use("/comments", commentRouter);
