0f06322e84265d13a7e5714acf763128eae96076
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-08 15:06:55 +0800

    move cors middleware above route definitions

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,14 +4,16 @@ import express from "express";
 import logger from "morgan";
 import path from "path";
 import "regenerator-runtime/runtime";
-import commentRouter from "./routes/commentRouter";
-import requestRouter from "./routes/requestRouter";
-import userRouter from "./routes/userRouter";
 
 var app = express();
 
 app.use(logger("dev"));
 app.use(cors({ origin: "https://kampung-cloud-prod.web.app" }));
+
+var commentRouter = require("./routes/commentRouter");
+var requestRouter = require("./routes/requestRouter");
+var userRouter = require("./routes/userRouter");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
