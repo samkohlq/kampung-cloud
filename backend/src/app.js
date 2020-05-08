985488cb1bbb05656f7c2818296bf5cd4f48b34e
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-08 15:13:10 +0800

    configure cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,23 +4,24 @@ import express from "express";
 import logger from "morgan";
 import path from "path";
 import "regenerator-runtime/runtime";
-
-var app = express();
-
-app.use(logger("dev"));
-app.use(cors({ origin: "https://kampung-cloud-prod.web.app" }));
-
 var commentRouter = require("./routes/commentRouter");
 var requestRouter = require("./routes/requestRouter");
 var userRouter = require("./routes/userRouter");
 
+var app = express();
+var corsOptions = {
+  origin: "https://kampung-cloud-prod.web.app",
+  optionsSuccessStatus: 200,
+};
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/users", userRouter);
-app.use("/requests", requestRouter);
-app.use("/comments", commentRouter);
+app.use("/users", cors(corsOptions), userRouter);
+app.use("/requests", cors(corsOptions), requestRouter);
+app.use("/comments", cors(corsOptions), commentRouter);
 
 module.exports = app;
