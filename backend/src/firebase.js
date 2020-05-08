dbfab6f8eaf6280e83800a1a4a675da83fb034ac
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-08 15:42:21 +0800

    use heroku environment variables rather than ServiceAccountKey.json

--- backend/src/firebase.js
+++ backend/src/firebase.js
@@ -1,9 +1,10 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./CCServiceAccountKey.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 export default admin;
