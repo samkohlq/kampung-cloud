4da297c89bf3f7f051035ce72b8d073b8a49e58e
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-08 15:01:22 +0800

    using cors, allow requests with frontend origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,29 +11,11 @@ import userRouter from "./routes/userRouter";
 var app = express();
 
 app.use(logger("dev"));
-app.use(cors());
+app.use(cors({ origin: "https://kampung-cloud-prod.web.app" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://kampung-cloud-prod.web.app"
-  );
-  // request methods to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  // request headers to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  next();
-});
 
 app.use("/users", userRouter);
 app.use("/requests", requestRouter);
