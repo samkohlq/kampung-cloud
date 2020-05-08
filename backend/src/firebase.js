e9d1757cd166b59632d0a3d98d8b1c72c919a3e2
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-08 15:50:49 +0800

    add firebase project id to firebase.js

--- backend/src/firebase.js
+++ backend/src/firebase.js
@@ -2,6 +2,7 @@ var admin = require("firebase-admin");
 
 admin.initializeApp({
   credential: admin.credential.cert({
+    project_id: process.env.FIREBASE_PROJECT_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
