746cced8cfac2972ac5dbbb47a61dbd7e68ab263
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-12 23:34:08 +0800

    update firebase config env variable names

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_CONFIG_API_KEY,
-  authDomain: process.env.FIREBASE_CONFIG_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_CONFIG_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_CONFIG_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_CONFIG_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
